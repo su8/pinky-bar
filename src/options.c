7d5546b643626e071db739cca4c614beb854198a
su8 <wifiextender2@gmail.com>
2018-04-16 13:44:48 +0200

    Add support for comments in the configuration file starting with /* comment */

--- src/options.c
+++ src/options.c
@@ -638,7 +638,7 @@ parse_konf(char *combined) {
         continue;
       }
     }
-    if ('#' == *ptr || '/' == *ptr || ';' == *ptr) {
+    if ('#' == *ptr || '/' == *ptr || ';' == *ptr || '*' == *ptr) {
       continue;
     }
     ello[1] = ptr;
